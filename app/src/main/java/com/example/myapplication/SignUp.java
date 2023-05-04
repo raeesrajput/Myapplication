52c6c2ef7bed540683b2d12aa652427659a02064
raees <mraeesafzal279@gmail.com>
2023-05-04 14:36:14 -0700

    System.out.println("this is testing");

---
+++ app/src/main/java/com/example/myapplication/SignUp.java
@@ -0,0 +1,30 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SignUp extends AppCompatActivity {
+    TextView signIn;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+      signIn= findViewById(R.id.tv_sign_up_sign_in);
+
+
+        signIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(SignUp.this, Login.class );
+                startActivity(i);
+             //   finish();
+            }
+        });
+        }
+    }
